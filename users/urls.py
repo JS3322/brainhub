7e85f910a6e21c4da977d5ea48b9c16c7fc06942
i30203322 <i30203322@gmail.com>
2020-03-02 17:22:41 +0900

    27-1

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,12 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.log_out, name="logout"),
-    path("sigup", views.SignUpView.as_view(), name="signup"),
-    path("verify/<str:key>", views.complete_verification, name="complete-verification"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.log_out, name="logout"),
+    path("sigup/", views.SignUpView.as_view(), name="signup"),
+    path(
+        "verify/<str:key>/", views.complete_verification, name="complete-verification"
+    ),
+    path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
 ]
